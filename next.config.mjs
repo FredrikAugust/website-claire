58b79d6a9ff662713d329144d650abb3e64959ab
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2025-11-12 00:30:34 +0100

    update remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
+const s3 = new URL(process.env.S3_ENDPOINT)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
   output: 'standalone',
-  images: { domains: [new URL(process.env.S3_ENDPOINT).hostname] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: s3.protocol.slice(0, -1),
+        hostname: s3.hostname,
+        pathname: '/api/media/file/**',
+      },
+    ],
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
