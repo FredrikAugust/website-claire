f86898abe502e70ba157de5f20ee681033f43ccc
Fredrik A. Madsen-Malmo <fredrik.malmo@icloud.com>
2026-03-08 22:27:13 +0100

    fix: remove max conns.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -38,7 +38,6 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI!,
-      max: 1,
       ssl: {
         rejectUnauthorized: false,
       },
